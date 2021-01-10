22a4c797e305af14913d38d097339bb4627e36a1
LiteHell <litehell@litehell.info>
2021-01-11 03:09:45 +0900

    Add reload link in error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -8,7 +8,9 @@
     <br />
     <p>
       자세한 사항은 회장단에게 문의해주세요.<br />
-      <a href="#" @click.prevent="$router.go(-1)">이전 페이지로 되돌아가기</a>
+      <a href="#" @click.prevent="$router.go(-1)">이전 페이지로 되돌아가기</a
+      ><br />
+      <a href="#" @click.prevent="reload">새로고침하기</a>
     </p>
   </div>
 </template>
@@ -22,6 +24,11 @@ export default Vue.extend({
   created() {
     this.$store.commit('SET_RIGHT_CARD_LAYOUT_TITLE', '오류')
     this.$store.commit('TOGGLE_ERROR_RIGHT_CARD_LAYOUT', true)
+  },
+  methods: {
+    reload() {
+      location.reload()
+    },
   },
 })
 </script>
