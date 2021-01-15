828bb3cce6b27d5f9964d5589cfa92c36a16899f
LiteHell <litehell@litehell.info>
2021-01-15 23:43:07 +0900

    Add OAuth2 Interaction page

---
+++ pages/interaction/_interactionId.vue
@@ -0,0 +1,27 @@
+<template>
+  <loading-modal active text="잠시만 기다려주세요..." />
+</template>
+
+<script lang="ts">
+import gql from 'graphql-tag'
+import Vue from 'vue'
+
+export default Vue.extend({
+  async created() {
+    const { interactionId } = this.$route.params
+    const result = await this.$apollo.mutate({
+      mutation: gql`
+        mutation($interactionId: String!) {
+          processOAuth2Client(interactionId: $interactionId)
+        }
+      `,
+      variables: {
+        interactionId,
+      },
+    })
+
+    location.assign(result.data.processOAuth2Client)
+    this.$store.commit('SET_RIGHT_CARD_LAYOUT_TITLE', '비밀번호 재설정')
+  },
+})
+</script>
