009c8da65a80ecd67d334d77ade998605c4f7158
LiteHell <litehell@litehell.info>
2021-01-20 21:58:23 +0900

    process logout with server

--- pages/logout.vue
+++ pages/logout.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script lang="ts">
+import gql from 'graphql-tag'
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -15,10 +16,29 @@ export default Vue.extend({
   },
   created() {
     this.$store.commit('SET_RIGHT_CARD_LAYOUT_TITLE', '로그아웃')
-    this.$apolloHelpers.onLogout().then(() => {
+  },
+  async mounted() {
+    if (this.$apolloHelpers.getToken()) {
+      await this.$apollo.mutate({
+        mutation: gql`
+          mutation($token: String!) {
+            logout(token: $token)
+          }
+        `,
+        variables: {
+          token: this.$apolloHelpers.getToken(),
+        },
+      })
+      this.$apolloHelpers.onLogout().then(() => {
+        this.goRedirect()
+      })
+    } else this.goRedirect()
+  },
+  methods: {
+    goRedirect() {
       if (/^https?:\/\//.test(this.redirect)) location.assign(this.redirect)
       else this.$router.push(this.redirect)
-    })
+    },
   },
 })
 </script>
